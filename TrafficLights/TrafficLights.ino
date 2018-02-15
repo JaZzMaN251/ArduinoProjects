caef09b1950f85017beea234827b357d2d7db1d4
JaZzMaN251 <Nick@MacBook-Pro-van-Nick.local>
2018-02-15 14:46:07 +0100

    TrafficLights v0.1
    working

---
+++ TrafficLights/TrafficLights.ino
@@ -0,0 +1,104 @@
+/* Traffic light system where one direction is normally open, and the other only when triggered by a push button.
+ * By Nick Puylaert.
+ */
+
+const int button = 8; //init button
+const int stateLed = 13; //init state led (to show if button has been pressed)
+int state;
+
+// init traffic light 1
+const int red1 = 2;
+const int yellow1 = 3; 
+const int green1 = 4;
+
+// traffic light 2
+const int red2 = 5; // init red1
+const int yellow2 = 6; // init yellow1
+const int green2 = 7; // init green 1
+
+void setup() {
+  // put your setup code here, to run once:
+// initialize serial communication at 9600 bits per second:
+Serial.begin(9600);
+pinMode(button, INPUT); // button is input
+pinMode(stateLed, OUTPUT);
+pinMode(red1, OUTPUT);
+pinMode(yellow1, OUTPUT);
+pinMode(green1, OUTPUT);
+pinMode(red2, OUTPUT);
+pinMode(yellow2, OUTPUT);
+pinMode(green2, OUTPUT);
+
+// init state
+state = 0;
+
+
+// init traffic light 1 -> GREEN
+digitalWrite(red1, LOW);
+digitalWrite(yellow1, LOW);
+digitalWrite(green1, HIGH);
+
+// init traffic light 2 -> RED
+digitalWrite(red2, HIGH);
+digitalWrite(yellow2, LOW);
+digitalWrite(green2, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+state = digitalRead(button); 
+  
+  // when button is pressed turn state into 1 and light onboardled)
+while (state == LOW) {
+  digitalWrite(stateLed, LOW);
+  Serial.println(state);
+  delay(1);
+  state = digitalRead(button);  
+}
+
+  state = HIGH;
+  digitalWrite(stateLed, HIGH);
+  Serial.println(state);
+  delay(1000);
+
+// check if button is pressed
+
+  digitalWrite(green1, LOW);
+  digitalWrite(yellow1, HIGH);
+  delay(1000);
+  digitalWrite(yellow1, LOW);
+
+  digitalWrite(red1, HIGH);
+  delay(2000);
+
+  digitalWrite(red2, LOW);
+  digitalWrite(yellow2, HIGH);
+  delay(1000);
+
+  digitalWrite(yellow2, LOW);
+  digitalWrite(green2, HIGH);
+  delay(2000);
+
+  digitalWrite(green2, LOW);
+  digitalWrite(yellow2, HIGH);
+  delay(1000);
+
+  digitalWrite(yellow2, LOW);
+  digitalWrite(red2, HIGH);
+  delay(2000);
+
+  digitalWrite(red1, LOW);
+  digitalWrite(yellow1, HIGH);
+  delay(1000);
+
+  digitalWrite(yellow1, LOW);
+  digitalWrite(green1, HIGH);
+  delay(2000);
+// reset state to 0 and led off
+
+state = 0;
+digitalWrite(stateLed, LOW);
+  Serial.println(state);
+  delay(1);    
+}
